dc7441b0c96f82bc1c4a29d230050855b7dfcb4f
Lydie Cherilus <lydiecherilus@yahoo.fr>
2020-12-17 14:24:03 -0500

    add markers

--- bus/src/Map.js
+++ bus/src/Map.js
@@ -1,11 +1,14 @@
-import React, { useState, useRef, useCallback } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import useSwr from 'swr';
 import './App.css'
 
 
 ReactMapGL.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 
 function Map() {
+
+    /** initialize map */
     const [viewport, setViewport] = useState({
         width: '100vw',
         height: '100vh',
@@ -14,7 +17,20 @@ function Map() {
         zoom: 11,
     });
 
-    /** initialize map */
+     /** set state */
+    const [busData, setBusData] = useState([]);
+
+     /** fetch data from Miami Transit API and update state */
+    const { data, error } = useSwr(`http://miami-transit-api.herokuapp.com/api/Buses.json`,
+        (...args) => fetch(...args)
+            .then(res => res.json()));
+            
+    useEffect(() => {
+        if (data) setBusData(data.RecordSet.Record);
+    }, [data]);
+
+
+    /** add zoom and movement functionality to map */
     const mapRef = useRef();
     const handleViewportChange = useCallback(
         (newViewport) => setViewport(newViewport),
@@ -29,7 +45,22 @@ function Map() {
                 mapboxApiAccessToken={ReactMapGL.accessToken}
                 onViewportChange={handleViewportChange}
                 mapStyle='mapbox://styles/mapbox/streets-v11'
-            ></ReactMapGL>
+            >
+                {/* add a marker for each bus on the map */}
+                {busData.map((bus, idx) => {
+                    return (
+                        <Marker
+                            latitude={parseFloat(bus.Latitude)}
+                            longitude={parseFloat(bus.Longitude)}
+                            key={idx}
+                        >
+                            <button>
+                                <img src='/bus-svg.html' alt='bus' />
+                            </button>
+                        </Marker>
+                    );
+                })}
+            </ReactMapGL>
         </div>
     );
 }
